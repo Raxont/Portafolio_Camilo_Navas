fd3eeaae07f93683323d9b4258afa19fb593c069
Raxont <raxonti@gmail.com>
2026-06-19 06:41:03 -0500

    refactor: :recycle: Se elimina la logica que no se estaba usando o estaba obsoleta, ademas se actualiza con los nuevos conocimientos y experiencias vividas

--- api/server.cjs
+++ api/server.cjs
@@ -1,5 +1,4 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -12,35 +11,6 @@ app.use(cors({
       allowedHeaders: ["Content-Type", "Authorization", "x-version"], 
 }));
 app.use(bodyParser.json());
-
-// Configuración de Nodemailer
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS   
-    }
-});
-
-// Ruta para manejar el envío de correos
-app.post('/send-email', (req, res) => {
-    const { user_name, user_email, subject, message, user_phone } = req.body;
-
-    const mailOptions = {
-        from: user_email,
-        to: process.env.EMAIL_USER, 
-        subject: subject,
-        text: `Nombre: ${user_name}\nCorreo: ${user_email}\nTeléfono: ${user_phone}\nMensaje: ${message}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            return res.status(500).json({ message: 'Error al enviar el mensaje' });
-        }
-        res.status(200).json({ message: 'Mensaje enviado correctamente' });
-    });
-});
 
 // Iniciar el servidor
 app.listen(puertoBack, () => {
